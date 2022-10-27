3846fe5c53a3f77a0d7355ded43d5cc77e779792
silvia pasini <silvia.pasini03@gmail.com>
2022-10-27 18:05:27 +0200

    seeds done for classes and section added

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,17 @@
 class LessonsController < ApplicationController
+
+  def show
+    set_lesson
+  end
+
+  private
+
+  def set_lesson
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def car_params
+    params.require(:lesson).permit( :description, :name, :online, :address, :price, :level, :when)
+  end
+
 end
